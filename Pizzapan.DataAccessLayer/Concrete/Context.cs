77ac62d0606183fa69281bf0a9e404f75f9ebb19
ilayda <ilaydaozken@gmail.com>
2023-07-13 10:57:49 +0300

    Add project files.

---
+++ Pizzapan.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pizzapan.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizzapan.DataAccessLayer.Concrete
+{
+    public class Context :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-AKOHCIC;initial catalog=CasgemDbPizzaPan;;initegrated security=true");
+        }
+       public DbSet<Category> Categories   { get; set; }
+       public DbSet<Contact> Contacts { get; set; }
+       public DbSet<Product> Products { get; set; }
+       public DbSet<Testimoial> Testimoials { get; set; }
+    }
+}
---
+++ Pizzapan.EntityLayer/Concrete/Testimoial.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pizzapan.EntityLayer.Concrete
+{
+    public class Testimoial
+    {
+        public int TestimoialID { get; set; }
+        public string CustomerName { get; set; }
+
+        [StringLength(50)]
+        public string CustomerTitle { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
