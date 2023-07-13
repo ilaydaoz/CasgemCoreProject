c6717e6e06a6442dcdb3a459f6ec03bf2d881d9a
ilayda <ilaydaozken@gmail.com>
2023-07-13 13:22:29 +0300

    bussines katmanı oluşturuldu

---
+++ Pizzapan.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using Pizzapan.DataAccessLayer.Abstract;
+using Pizzapan.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizzapan.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+
+        public void Delete(T entity)
+        {
+            using var context = new Context();
+            context.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            using var context = new Context();
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            using var context = new Context();
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T entity)
+        {
+            using var context = new Context();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            using var context = new Context();
+            context.Update(entity);
+            context.SaveChanges();
+        }
+    }
+}
