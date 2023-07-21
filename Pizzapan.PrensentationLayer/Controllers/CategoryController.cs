abc5420c0429fcc67dbe9ee54e0244a3f1ca7c27
ilayda <ilaydaozken@gmail.com>
2023-07-21 15:25:27 +0300

    category

--- Pizzapan.PrensentationLayer/Controllers/CategoryController.cs
+++ Pizzapan.PrensentationLayer/Controllers/CategoryController.cs
@@ -2,29 +2,25 @@
 using Pizzapan.BussinesLayer.Absrtact;
 using Pizzapan.EntityLayer.Concrete;
 
-namespace Pizzapan.PrensentationLayer.Controllers
+namespace Pizzapan.PresentationLayer.Controllers
 {
     public class CategoryController : Controller
     {
         private readonly ICategoryService _categoryService;
-
         public CategoryController(ICategoryService categoryService)
         {
             _categoryService = categoryService;
         }
-
         public IActionResult Index()
         {
             var values = _categoryService.TGetList();
             return View(values);
         }
-
         [HttpGet]
         public IActionResult AddCategory()
         {
             return View();
         }
-
         [HttpPost]
         public IActionResult AddCategory(Category category)
         {
@@ -33,8 +29,8 @@ namespace Pizzapan.PrensentationLayer.Controllers
         }
         public IActionResult DeleteCategory(int id)
         {
-           var value = _categoryService.TGetByID(id);
-            _categoryService.TDelete(value);    
+            var value = _categoryService.TGetByID(id);
+            _categoryService.TDelete(value);
             return RedirectToAction("Index");
         }
         [HttpGet]
@@ -43,7 +39,6 @@ namespace Pizzapan.PrensentationLayer.Controllers
             var value = _categoryService.TGetByID(id);
             return View(value);
         }
-
         [HttpPost]
         public IActionResult UpdateCategory(Category category)
         {
